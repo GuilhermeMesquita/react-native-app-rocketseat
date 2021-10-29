9ac54428448e17ec217b82ac16da770ada5ad1a0
Guilherme Mesquita Rocha <guilhermemesquitarch@gmail.com>
2021-10-28 21:49:36 -0300

    added initial configs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+// Não possuem valores semânticos (significado)
+// Não possuem estilização própria
+// View: div, footer, header, main, aside, section
 
 export default function App() {
-    return <View />;
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1"></StatusBar>
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello world</Text>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold'
+    }
+});
